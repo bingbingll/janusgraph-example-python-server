e6afc1ce917f9507c1a76ea838358d2678b787f1
bing.li <ibinbin68@163.com>
2019-09-12 15:23:59 +0800

    返回json数据

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from typing import Dict, List, Any
+
 from flask import jsonify
 from flask import json
 from flask import Flask
+from gremlin_python.structure.graph import Vertex
 from config import gremlinConfig
-from gremlin_python.process.traversal import T
 
 app = Flask(__name__)
 
@@ -12,24 +14,33 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/getList', methods=['GET'])
-def get_list():
+@app.route('/getList1', methods=['GET'])
+def get_list_1():
+    g = gremlinConfig.get_graph()
+    vl = g.V().toList()
+    result = []  ## 空列表
+    for Vertex in vl:
+        # 空的字典对象
+        dict_ver = {}
+        dict_ver['id'] = Vertex.id
+        dict_ver['label'] = Vertex.label
+        result.append(dict_ver)
+    return jsonify(result)
+
+
+@app.route('/getList2', methods=['GET'])
+def get_list_2():
     g = gremlinConfig.get_graph()
     vs = g.V().valueMap(True).toList()
     print(vs)
-    # 目前不知道该怎么将vs转换为json数据返回
-    return '111'
+    result = []  ## 空列表
+    for dic in vs:
+        dict2 = {}  # 创建新的字典
+        for key, value in dic.items():
+            dict2.setdefault(str(key), value)
+        result.append(dict2)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# list 转成Json格式数据
-def listToJson(lst):
-    import json
-    import numpy as np
-    keys = [str(x) for x in np.arange(len(lst))]
-    list_json = dict(zip(keys, lst))
-    str_json = json.dumps(list_json, indent=2, ensure_ascii=False)  # json转为string
-    return str_json
